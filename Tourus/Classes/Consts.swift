6cf37ab30373359c2abffc6608c2f893ce062c2b
Alice <aliceka495@gmail.com>
2019-01-03 22:39:51 +0200

    check

---
+++ Tourus/Classes/Consts.swift
@@ -0,0 +1,37 @@
+//
+//  Consts.swift
+//  Tripit
+//
+//  Created by Raz Vaknin on 29 Kislev 5779.
+//  Copyright © 5779 razop. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Consts{
+    struct Names {
+        static let UserInfoTableName: String = "Users"
+        static let ImagesFolderName: String = "ImagesStorage"
+        static let ProfileImagesFolderName: String = "ProfileImagesStorage"
+    }
+    
+    struct General {
+        static func convertTimestampToStringDate(_ serverTimestamp: Double, _ format:String = "dd/MM/yyyy HH:mm") -> String {
+            let x = serverTimestamp / 1000
+            let date = NSDate(timeIntervalSince1970: x)
+            let formatter = DateFormatter()
+            formatter.dateFormat = format
+            
+            return formatter.string(from: date as Date)
+        }
+        
+        static func getCancelAlertController(title:String, messgae:String, buttonText:String = "Dismiss") -> UIAlertController
+        {
+            let alertController = UIAlertController(title: title, message: messgae, preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: buttonText, style: UIAlertAction.Style.cancel, handler: nil))
+            
+            return alertController
+        }
+    }
+}
--- Tourus/Models/FirebaseModel.swift
+++ Tourus/Models/FirebaseModel.swift
@@ -13,10 +13,106 @@ import UIKit
 
 class FirebaseModel {
     var databaseRef: DatabaseReference!
+    lazy var storageRef = Storage.storage().reference(forURL:
+        "gs://org-tourus-acb4d.appspot.com")
     
     init() {
         FirebaseApp.configure()
         databaseRef = Database.database().reference()
+         self.databaseRef!.child(Consts.Names.UserInfoTableName).child("ss").setValue("")
+        //completionBlock(true)
+        //self.databaseRef!.child("jjd").setValue("hello")
+      //  self.databaseRef!.child("jj").setValue("hello")
+    }
+    
+    func addUserInfo(_ userInfo:UserInfo, _ image:UIImage?, _ completionBlock:@escaping (Bool) -> Void = {_  in}) {
+       
+        
+        if image != nil {
+            saveImage(folderName: Consts.Names.ProfileImagesFolderName, image: image!) { (url:String?) in
+                if url != nil {
+                    userInfo.profileImageUrl = url!
+                }
+                self.databaseRef!.child(Consts.Names.UserInfoTableName).child(userInfo.uid).setValue(userInfo.toJson())
+                completionBlock(true)
+            }
+        }
+        else {
+            self.databaseRef!.child(Consts.Names.UserInfoTableName).child(userInfo.uid).setValue(userInfo.toJson())
+            completionBlock(true)
+        }
+    }
+    
+    func saveImage(folderName:String, image:UIImage, callback:@escaping (String?) -> Void) {
+        let data = image.jpegData(compressionQuality: 0.8)
+        let imageName = "\(Date().timeIntervalSince1970).jpg"
+        let imageRef = storageRef.child(folderName).child(imageName)
+        
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+        
+        imageRef.putData(data!, metadata: metadata) { (metadata, error) in
+            imageRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    return
+                }
+                print("url: \(downloadURL)")
+                callback(downloadURL.absoluteString)
+            }
+        }
+    }
+    
+    func getUserInfo(_ uid:String, callback:@escaping (UserInfo?) -> Void) {
+        self.databaseRef!.child(Consts.Names.UserInfoTableName).child(uid).observeSingleEvent(of: .value, with: {
+            (snapshot) in
+            
+            if snapshot.exists() {
+                let value = snapshot.value as! [String:Any]
+                let userInfo = UserInfo(_uid: uid, json: value)
+                
+                callback(userInfo)
+            }
+            else {
+                callback(nil)
+            }
+        })
+    }
+    
+    
+    func signUp(_ email:String, _ password:String, _ callback:@escaping (Bool) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+            if authResult?.user != nil {
+                
+                let email = authResult!.user.email!
+                let display = (email.components(separatedBy: "@"))[0]
+                
+                let userInfo = UserInfo(_uid: authResult!.user.uid, _displayName: display, _email: email, _profileImageUrl: nil)
+                self.addUserInfo(userInfo, nil, { (val) in
+                    print("1")
+                    callback(true)
+                })
+            }
+            else {
+                print("0")
+                callback(false)
+            }
+        }
+    }
+    
+    func signIn(_ email:String, _ password:String, _ callback:@escaping (Bool) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if (user != nil  ) {
+                callback(true)
+            }
+            else {
+                callback(false)
+            }
+        }
+    }
+    
+    func currentUser() -> User? {
+        return Auth.auth().currentUser
     }
 }
 
