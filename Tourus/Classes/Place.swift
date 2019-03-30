3bb605cce753cc0efff9b6301df8c5862be10a73
Baruch Yahalom <baruch5551@gmail.com>
2019-03-30 20:33:33 +0300

    Add function to fetch nearby places using google api

--- Tourus/Classes/Place.swift
+++ Tourus/Classes/Place.swift
@@ -13,11 +13,28 @@ class Place {
     var name:String = ""
     var address:String = ""
     var picturesUrls:[String]? = nil
+    var rating: Double? = nil
+    var ratingsAmount: Int? = nil
+    var priceLevel: Int? = nil
+    var types: [String]? = nil
+    var isOpen: Bool? = nil
 
     init(googlePlace:GMSPlace?) {
         if googlePlace != nil {
             name = googlePlace!.name!
             address = ((googlePlace!.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n"))!)
+        }
+    }
+    
+    init(googlePlace:GooglePlace?) {
+        if googlePlace != nil {
+            name = googlePlace!.name ?? ""
+            address = String(googlePlace!.geometry.location.lat) + "," + String(googlePlace!.geometry.location.lng)
+            rating = googlePlace?.rating
+            ratingsAmount = googlePlace?.user_ratings_total
+            priceLevel = googlePlace?.price_level
+            types = googlePlace?.types
+            isOpen = googlePlace?.opening_hours?.open_now
         }
     }
 }
