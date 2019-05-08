0d1d414d3974265ddb1c3d62b4aa52930457b967
Alice <aliceka495@gmail.com>
2019-05-08 14:58:30 +0300

    added checkmark table view cell
    added checkmark table view cell

---
+++ Tourus/Controllers/CheckMarkViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Checkmark
+//
+//  Created by Alice on 21/12/18.
+//  Copyright © 2018 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+import UIKit
+
+class CheckMarkViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
+    var languages = ["C#", "Pyhton", "Javascript", "Swift", "C++", "C"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return languages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = languages[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            languages.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
--- Tourus/Controllers/SettingsViewController.swift
+++ Tourus/Controllers/SettingsViewController.swift
@@ -8,7 +8,44 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    var languages = ["Pool", "Food", "Shopping", "Car Racing", "Gym", "Club"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return languages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = languages[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCell.AccessoryType.checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+        else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCell.EditingStyle.delete
+        {
+            languages.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
+    }
+    
     
     @IBOutlet var profileImage: UIImageView!
     @IBOutlet var userNameLabel: UILabel!
